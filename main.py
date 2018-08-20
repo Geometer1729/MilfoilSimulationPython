312eb3ba501184fa6f5b036d008390240d83c9d7
Geometer1729 <kuhnsbt@clarkson.edu>
2018-08-20 15:16:09 -0400

    implement new model create notebook thingy

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
 from interpilation import interpilate
 from SimChain import Chain
-
+from phases import Growth , Winter
 from RungeKuta import buildODEPhase , timeCon
-from iteratedFunc      import buildIter
+from iteratedFunc import buildIter
 import matplotlib.pyplot as plt
-import numpy             as np
+import numpy as np
+from itertools import chain
+from ipywidgets import interactive, fixed
+import ipywidgets as widgets
+from matplotlib.pyplot import figure
 
-tau = 2*np.pi
+tol = 0.001
+maxStep = 0.05
+winterTime=0.1
 
+def plotFrom(growthTime,seasons,startC):
+    testFrame = (0,[(0,0),(startC,0)])
+    testTimes = np.arange(0,seasons*(growthTime+winterTime),maxStep)
+    GrowthPhase = lambda n: buildODEPhase(tol,maxStep,Growth,timeCon(n*growthTime+(n-1)*winterTime))
+    WinterPhase = buildIter(winterTime,Winter)
+    Phases= list(chain.from_iterable((GrowthPhase(x), WinterPhase) for x in range(1,seasons+1) ))
+    Env = Chain(testFrame,Phases)
+    testData = interpilate(testTimes,Env)
+    t,s=Env.frame
+    ms,cs = s
+    m,dm = ms
+    c,dc = cs
+    plt.plot(testTimes,testData)
+    plt.show()
+    print("end carbs :",c)
 
-
-tol = 0.01
-maxStep = 0.01
-
-testFrame = (0,[(1,0),(0,1)])
-testSystem1 = lambda t,x : [-1*x[1],x[0]] # simple circular orbit
-testSystem2 = lambda t,xs : [-t*x for x in xs] # acelerating "nearly exponential" decay
-testFunction1 = lambda t,xs: (x+t/10 for x in xs)
-testPhase1 = buildODEPhase(tol,maxStep,testSystem1,timeCon(tau))
-testPhase2 = buildODEPhase(tol,maxStep,testSystem2,timeCon(10))
-testPhase3 = buildIter(0.1,testFunction1)
-testPhase4 = buildODEPhase(tol,maxStep,testSystem1,timeCon(10+tau))
-testChain = Chain(testFrame,[testPhase1,testPhase2,testPhase3,testPhase4])
-testTimes = np.arange(0,10+tau,0.05)
-testData = interpilate(testTimes,testChain)
-
-plt.plot(testTimes,testData)
-plt.show()
+interactive(plotFrom,growthTime=(0.01,10),seasons=(0,10),startC=widgets.FloatSlider(value=0.5,min=0.001,max=1,step=0.001))
